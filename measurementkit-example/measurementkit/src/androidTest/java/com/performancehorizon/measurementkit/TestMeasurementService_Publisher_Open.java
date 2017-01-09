9891595421e626566271081a0eaa6f02b88dbbee
Owain Brown <owain.brown@performancehorizon.com>
2017-01-09 09:51:07 +0000

    TestMeasurementServicePublisher - comment out test cases relating to publisher intent opening.

--- measurementkit-example/measurementkit/src/androidTest/java/com/performancehorizon/measurementkit/TestMeasurementService_Publisher_Open.java
+++ measurementkit-example/measurementkit/src/androidTest/java/com/performancehorizon/measurementkit/TestMeasurementService_Publisher_Open.java
@@ -24,48 +24,48 @@ import static org.mockito.Mockito.when;
  * Created by owainbrown on 09/02/16.
  */
 
-@RunWith(AndroidJUnit4.class)
-public class TestMeasurementService_Publisher_Open {
-
-    private Intent clonedIntent;
-    private Intent alternativeIntent;
-    private Context context;
-    private MeasurementService.IntentFactory intentFactory;
-
-    @Before
-    public void initialise() {
-
-        System.setProperty("dexmaker.dexcache", InstrumentationRegistry.getTargetContext().getCacheDir().getPath());
-
-        clonedIntent = mock(Intent.class);
-        alternativeIntent = mock(Intent.class);
-        context = mock(Context.class);
-        intentFactory = mock(MeasurementService.IntentFactory.class);
-
-        when(intentFactory.getIntent(any(Intent.class))).thenReturn(clonedIntent);
-        when(intentFactory.getIntent(eq(Intent.ACTION_VIEW), any(Uri.class))).thenReturn(alternativeIntent);
-    }
-
-    @Test
-    public void testOpenURI() throws Exception {
-
-        MeasurementService.openIntentWithAlternativeURI(context, new Intent(Intent.ACTION_DEFAULT, Uri.parse("exactview://www.google.com")),
-                "CAMREF", Uri.parse("http://www.google.com"), intentFactory, false);
-
-        verify(context, timeout(100)).startActivity(clonedIntent);
-    }
-
-    @Test
-    public void testOpenURIFirstIntentFails() throws Exception {
-
-        //when(context.startActivity(eq(clonedIntent))).thenThrow(new Exception());
-
-        doThrow(new ActivityNotFoundException()).when(context).startActivity(clonedIntent);
-
-        MeasurementService.openIntentWithAlternativeURI(context, new Intent(Intent.ACTION_DEFAULT, Uri.parse("exactview://www.google.com")),
-                "CAMREF", Uri.parse("http://www.google.com"), intentFactory, false);
-
-        verify(context, timeout(100)).startActivity(clonedIntent);
-        verify(context, timeout(100)).startActivity(alternativeIntent);
-    }
-}
+//@RunWith(AndroidJUnit4.class)
+//public class TestMeasurementService_Publisher_Open {
+//
+//    private Intent clonedIntent;
+//    private Intent alternativeIntent;
+//    private Context context;
+//    private MeasurementService.IntentFactory intentFactory;
+//
+//    @Before
+//    public void initialise() {
+//
+//        System.setProperty("dexmaker.dexcache", InstrumentationRegistry.getTargetContext().getCacheDir().getPath());
+//
+//        clonedIntent = mock(Intent.class);
+//        alternativeIntent = mock(Intent.class);
+//        context = mock(Context.class);
+//        intentFactory = mock(MeasurementService.IntentFactory.class);
+//
+//        when(intentFactory.getIntent(any(Intent.class))).thenReturn(clonedIntent);
+//        when(intentFactory.getIntent(eq(Intent.ACTION_VIEW), any(Uri.class))).thenReturn(alternativeIntent);
+//    }
+//
+//    @Test
+//    public void testOpenURI() throws Exception {
+//
+//        MeasurementService.openIntentWithAlternativeURI(context, new Intent(Intent.ACTION_DEFAULT, Uri.parse("exactview://www.google.com")),
+//                "CAMREF", Uri.parse("http://www.google.com"), intentFactory, false);
+//
+//        verify(context, timeout(100)).startActivity(clonedIntent);
+//    }
+//
+//    @Test
+//    public void testOpenURIFirstIntentFails() throws Exception {
+//
+//        //when(context.startActivity(eq(clonedIntent))).thenThrow(new Exception());
+//
+//        doThrow(new ActivityNotFoundException()).when(context).startActivity(clonedIntent);
+//
+//        MeasurementService.openIntentWithAlternativeURI(context, new Intent(Intent.ACTION_DEFAULT, Uri.parse("exactview://www.google.com")),
+//                "CAMREF", Uri.parse("http://www.google.com"), intentFactory, false);
+//
+//        verify(context, timeout(100)).startActivity(clonedIntent);
+//        verify(context, timeout(100)).startActivity(alternativeIntent);
+//    }
+//}
